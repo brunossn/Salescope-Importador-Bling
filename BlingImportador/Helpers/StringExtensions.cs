8f4640aa7500a6058938f13ed4a81b82ee3d9545
Bruno Soares Nepomuceno <brunossn@gmail.com>
2018-09-18 11:16:19 -0300

    Remoção de usings

--- BlingImportador/Helpers/StringExtensions.cs
+++ BlingImportador/Helpers/StringExtensions.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlingImportador.Helpers {
+﻿namespace BlingImportador.Helpers
+{
     public static class StringExtensions{
         public static string Left(this string str, int length) {
             return str.Length < length ? str : str.Substring(0, length); 
